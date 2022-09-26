ce275295a062b92b9ced152c9084fca8714b0303
djbetamax <hugots23@gmail.com>
2022-09-26 11:34:45 -0300

    build: querySelector to divide elements and variable names

---
+++ app.js
@@ -0,0 +1,10 @@
+// * timers
+const hours = document.querySelector('.hours');
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+const miliseconds = document.querySelector('.miliSeconds');
+
+// * buttons
+const play = document.querySelector('.start');
+const pause = document.querySelector('.pause');
+const stop = document.querySelector('.stop');
